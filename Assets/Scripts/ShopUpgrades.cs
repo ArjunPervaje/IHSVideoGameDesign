d7422e1aa86d38bb2e68ac8e27bee095ac9b6f57
Pervaje <pervarj27@issaquah.wednet.edu>
2026-04-06 10:39:38 -0700

    Started Player UI

--- Assets/Scripts/ShopUpgrades.cs
+++ Assets/Scripts/ShopUpgrades.cs
@@ -3,101 +3,101 @@ using UnityEngine;
 
 public class ShopUpgrades : ScriptableObject
 {
-    public string itemName;
-    public Sprite icon;
-    public int cost;
-    public string description;
+    //public string itemName;
+    //public Sprite icon;
+    //public int cost;
+    //public string description;
 
-    public Button movementUpgrades;
-    public Button combatUpgrades;
-    public Button passiveUpgrades;
+    //public Button movementUpgrades;
+    //public Button combatUpgrades;
+    //public Button passiveUpgrades;
 
-    private List<string> movementUp = new List<string>
-    {
-        "Jump Power", "Dash", "Speed Up", "Wall Climb"
-    };
+    //private List<string> movementUp = new List<string>
+    //{
+    //    "Jump Power", "Dash", "Speed Up", "Wall Climb"
+    //};
 
-    private List<string> combatUp = new List<string>
-    {
-        "Shield", "Parry", "Attack Power"
-    };
+    //private List<string> combatUp = new List<string>
+    //{
+    //    "Shield", "Parry", "Attack Power"
+    //};
 
-    private List<string> passiveUp = new List<string>
-    {
-        "Add Health", "Invisibility", "Charm", "Equips"
-    };
+    //private List<string> passiveUp = new List<string>
+    //{
+    //    "Add Health", "Invisibility", "Charm", "Equips"
+    //};
 
 
-    void Start()
-    {
-        movementUpgrades.onClck.AddListener(() => OnNewButtonClick("movements"));
-        combatUpgrades.onClck.AddListener(() => OnNewButtonClick("combats"));
-        passiveUpgrades.onClck.AddListener(() => OnNewButtonClick("passives"));
-    }
+    //void Start()
+    //{
+    //    movementUpgrades.onClck.AddListener(() => OnNewButtonClick("movements"));
+    //    combatUpgrades.onClck.AddListener(() => OnNewButtonClick("combats"));
+    //    passiveUpgrades.onClck.AddListener(() => OnNewButtonClick("passives"));
+    //}
 
-    void OnButtonClick(string buttonName)
-    {
-        switch (buttonName)
-        {
+    //void OnButtonClick(string buttonName)
+    //{
+    //    switch (buttonName)
+    //    {
 
-            case "movements":
-                // clear old buttons
-                foreach (Transform child in buttonContainer)
-                    Destroy(child.gameObject);
+    //        case "movements":
+    //            // clear old buttons
+    //            foreach (Transform child in buttonContainer)
+    //                Destroy(child.gameObject);
 
-                // shuffle and pick 3
-                List<string> shuffled = GetShuffled(movementUp);
-                List<string> selected = shuffled.GetRange(0, 3);
+    //            // shuffle and pick 3
+    //            List<string> shuffled = GetShuffled(movementUp);
+    //            List<string> selected = shuffled.GetRange(0, 3);
 
-                foreach (string upgrade in selected)
-                {
-                    Button newButton = Instantiate(buttonPrefab, buttonContainer);
-                    newButton.GetComponentInChildren<Text>().text = upgrade;
+    //            foreach (string upgrade in selected)
+    //            {
+    //                Button newButton = Instantiate(buttonPrefab, buttonContainer);
+    //                newButton.GetComponentInChildren<Text>().text = upgrade;
 
-                    string upgradeName = upgrade;
-                    newButton.onClick.AddListener(() => SelectUpgrade(upgradeName));
-                }
-                break;
+    //                string upgradeName = upgrade;
+    //                newButton.onClick.AddListener(() => SelectUpgrade(upgradeName));
+    //            }
+    //            break;
 
-            case "combats":
-                // clear old buttons
-                foreach (Transform child in buttonContainer)
-                    Destroy(child.gameObject);
+    //        case "combats":
+    //            // clear old buttons
+    //            foreach (Transform child in buttonContainer)
+    //                Destroy(child.gameObject);
 
-                // shuffle and pick 3
-                List<string> shuffled = GetShuffled(combatUp);
-                List<string> selected = shuffled.GetRange(0, 3);
+    //            // shuffle and pick 3
+    //            List<string> shuffled = GetShuffled(combatUp);
+    //            List<string> selected = shuffled.GetRange(0, 3);
 
-                foreach (string upgrade in selected)
-                {
-                    Button newButton = Instantiate(buttonPrefab, buttonContainer);
-                    newButton.GetComponentInChildren<Text>().text = upgrade;
+    //            foreach (string upgrade in selected)
+    //            {
+    //                Button newButton = Instantiate(buttonPrefab, buttonContainer);
+    //                newButton.GetComponentInChildren<Text>().text = upgrade;
 
-                    string upgradeName = upgrade;
-                    newButton.onClick.AddListener(() => SelectUpgrade(upgradeName));
-                }
-                break;
+    //                string upgradeName = upgrade;
+    //                newButton.onClick.AddListener(() => SelectUpgrade(upgradeName));
+    //            }
+    //            break;
 
-            case "passives":
-                // clear old buttons
-                foreach (Transform child in buttonContainer)
-                    Destroy(child.gameObject);
+    //        case "passives":
+    //            // clear old buttons
+    //            foreach (Transform child in buttonContainer)
+    //                Destroy(child.gameObject);
 
-                // shuffle and pick 3
-                List<string> shuffled = GetShuffled(passiveUp);
-                List<string> selected = shuffled.GetRange(0, 3);
+    //            // shuffle and pick 3
+    //            List<string> shuffled = GetShuffled(passiveUp);
+    //            List<string> selected = shuffled.GetRange(0, 3);
 
-                foreach (string upgrade in selected)
-                {
-                    Button newButton = Instantiate(buttonPrefab, buttonContainer);
-                    newButton.GetComponentInChildren<Text>().text = upgrade;
+    //            foreach (string upgrade in selected)
+    //            {
+    //                Button newButton = Instantiate(buttonPrefab, buttonContainer);
+    //                newButton.GetComponentInChildren<Text>().text = upgrade;
 
-                    string upgradeName = upgrade;
-                    newButton.onClick.AddListener(() => SelectUpgrade(upgradeName));
-                }
-                break;
-        }
-    }
+    //                string upgradeName = upgrade;
+    //                newButton.onClick.AddListener(() => SelectUpgrade(upgradeName));
+    //            }
+    //            break;
+    //    }
+    //}
 
 
 }
